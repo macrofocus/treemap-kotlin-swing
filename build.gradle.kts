0deb7a962af67f438ba28e6b64f8ff95bdac67ba
Luc Girardin <luc.girardin@ethz.ch>
2021-11-18 09:40:18 +0100

    EFLUX-133 - Map unresolved lines

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,10 @@ repositories {
     maven { url = uri("https://www.macrofocus.com/archiva/repository/public/") }
     maven { url = uri("https://www.macrofocus.com/archiva/repository/snapshots/") }
     mavenCentral()
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev/")
+
+    google()
 }
 
 val frameworkAttribute = Attribute.of("mkui", String::class.java)
